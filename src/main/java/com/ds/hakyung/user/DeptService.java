64a8db4498fcada2a85b0d6a71aa8e377515937b
dbsgkrud5379 <dbsgkrud5379@naver.com>
2023-04-26 13:32:50 +0900

    final

--- src/main/java/com/ds/hakyung/user/DeptService.java
+++
@@ -1,9 +0,0 @@
-package com.ds.hakyung.user;
-
-import java.util.List;
-
-import com.ds.hakyung.user.domain.DeptDto;
-
-public interface DeptService {
-	List<DeptDto> getList(DeptDto dto);
-}
--- src/main/java/com/ds/hakyung/user/repository/DeptDao.java
+++
@@ -1,9 +0,0 @@
-package com.ds.hakyung.user.repository;
-
-import java.util.List;
-
-import com.ds.hakyung.user.domain.DeptDto;
-
-public interface DeptDao {
-	List<DeptDto> getList(DeptDto dto);
-}
